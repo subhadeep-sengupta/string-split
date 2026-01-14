c44f65bb6716052e469e9a026327b5027d3af214
Subhadeep Sengupta <www.subhadeepsengupta2002@gmail.com>
2026-01-14 20:10:09 +0530

    Implemented char's also

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 ## String Split
 
 creating a Implementation of Standard library's str::split(), which returns succesive substrings.
+
+### Example
+
+```rust
+StringSplit::new("Hello world, Here I am", ",")
+/// Output: [Hello world, Here I am]
+```
+
+### Local Development
+
+1. clone the repo `bash git clone https://github.com/subhadeep-sengupta/string-split.git`
+
+2. cd into the repo
+
+3. Build the project `bash cargo build`
+   For tests `bash cargo test`
+
+For further development feel free to open a PR!
--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,17 @@
 // !
-// #![warn(missing_docs, rust_2018_idioms, missing_debug_implementations)]
+#![warn(rust_2018_idioms)]
 #[derive(Debug)]
-pub struct StringSplit<'a> {
-    remainder: Option<&'a str>,
-    delimiter: &'a str,
+pub struct StringSplit<'haystack, D> {
+    remainder: Option<&'haystack str>,
+    delimiter: D,
 }
 
-impl<'a> StringSplit<'a> {
-    pub fn new(haystack: &'a str, delimiter: &'a str) -> Self {
+pub trait Delimiter {
+    fn find_next(&self, s: &str) -> Option<(usize, usize)>;
+}
+
+impl<'haystack, D> StringSplit<'haystack, D> {
+    pub fn new(haystack: &'haystack str, delimiter: D) -> Self {
         Self {
             remainder: Some(haystack),
             delimiter,
@@ -15,22 +19,46 @@ impl<'a> StringSplit<'a> {
     }
 }
 
-impl<'a> Iterator for StringSplit<'a> {
-    type Item = &'a str;
+impl<'haystack, D> Iterator for StringSplit<'haystack, D>
+where
+    D: Delimiter,
+{
+    type Item = &'haystack str;
 
     fn next(&mut self) -> Option<Self::Item> {
-        if let Some(ref mut remainder) = self.remainder {
-            if let Some(next_delim) = remainder.find(self.delimiter) {
-                let until_delim = &remainder[..next_delim];
-                *remainder = &remainder[(next_delim + self.delimiter.len())..];
-                Some(until_delim)
-            } else {
-                self.remainder.take()
-            }
+        let remainder = self.remainder.as_mut()?;
+        if let Some((delim_start, delim_end)) = self.delimiter.find_next(remainder) {
+            let until_delim = &remainder[..delim_start];
+            *remainder = &remainder[delim_end..];
+            Some(until_delim)
         } else {
-            None
+            self.remainder.take()
         }
     }
+}
+
+impl Delimiter for &str {
+    fn find_next(&self, s: &str) -> Option<(usize, usize)> {
+        s.find(self).map(|start| (start, start + self.len()))
+    }
+}
+impl Delimiter for char {
+    fn find_next(&self, s: &str) -> Option<(usize, usize)> {
+        s.char_indices()
+            .find(|(_, c)| c == self)
+            .map(|(start, _)| (start, self.len_utf8()))
+    }
+}
+
+fn until_chars(s: &str, c: char) -> &str {
+    StringSplit::new(s, c)
+        .next()
+        .expect("StringSplit always give some result")
+}
+
+#[test]
+fn until_chars_test() {
+    assert_eq!(until_chars("hello world", 'o'), "hell");
 }
 
 #[test]
